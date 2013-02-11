9166b7c0c5d84490a8d71ad15cc928073ec4feaf
Conner Brooks <conner@Conner-uThink.(none)>
2013-02-10 21:16:07 -0500

    fixed famisign ruby

--- famisign.rb
+++ famisign.rb
@@ -1,14 +1,16 @@
-# Twitter.configure do |config|
-#  config.consumer_key = 'kkBNhGcSFZ4kE4CRMmWcw'
-#  config.consumer_secret = 'C2GCFAL0UTTRUBnSglgn64BlG0jDK0vxyA86Isgm3A'
-#  config.oauth_token = '15356515-RdmmXpbzGZSQLddth18opg5qfPzolmWGf0LABZ8a7'
-#  config.oauth_token_secret = 'MRKbSA1Wglo4XmwLh1NeHn3bHfeqYuIVBAgy6ywGg'
-#end
-
-#tList = Twitter.user_timeline("familab")
-include 'betabrite'
+require 'rubygems'
+require 'betabrite'
 require 'serialport'
 require 'twitter'
+
+Twitter.configure do |config|
+  config.consumer_key = 'kkBNhGcSFZ4kE4CRMmWcw'
+  config.consumer_secret = 'C2GCFAL0UTTRUBnSglgn64BlG0jDK0vxyA86Isgm3A'
+  config.oauth_token = '15356515-RdmmXpbzGZSQLddth18opg5qfPzolmWGf0LABZ8a7'
+  config.oauth_token_secret = 'MRKbSA1Wglo4XmwLh1NeHn3bHfeqYuIVBAgy6ywGg'
+end
+
+tList = Twitter.user_timeline("familab")
 
 bb = BetaBrite::Serial.new('/dev/ttyUSB0') do |sign|
     sign.stringfile('0') do
